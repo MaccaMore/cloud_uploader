8478d90e651aa017a49c130453ea079a88ae57c9
MaccaMore <S3886682@student.rmit.edu.au>
2024-09-13 18:49:16 +1000

    Error checking before upload.

--- cloudupload.sh
+++ cloudupload.sh
@@ -3,16 +3,42 @@
 
 if [ $# -eq 0 ]
 then
-    echo "Enter the name of the file and bucket location to upload to"
+    echo "Syntax to use this script is: cloudupload.sh file_path destination"
 exit 1
 fi
 
 #if file does not exits:
 if [ ! -f $1 ]
 then
-echo "File does not exist"
+echo "File does not exist. Please enter a valid file path."
 exit 1
 fi
 
-
 file_path=$1
+destination=$2
+
+# Check if AWS CLI is installed
+if [ ! command -v aws &> /dev/null ]
+then
+    echo "AWS CLI could not be found. Please install AWS CLI."
+    exit 1
+fi
+
+# check logged in user
+# ~./aws/credentials file should be present with cloud-upload credentials
+if [ ! -f ~/.aws/credentials ]
+then
+echo "Please install AWS CLI, create user with S3 bucket permissions, and add as profile s3-access"
+exit 1
+fi
+
+# check cloud upload profile exists in credentials file
+
+if [ -z "$(grep "s3-access" ~/.aws/credentials)" ]
+then
+echo "Please create a AWS profile using the command: aws configure --profile s3-access"
+exit 1
+fi
+
+# check if destination bucket exists
+ aws s3 ls s3://$destination
